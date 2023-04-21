99c7dee1fb8e92db9ea0568511f06ea40a2ca058
Defelo <elodef42@gmail.com>
2023-04-21 14:25:29 +0200

    Add nix rust build

--- flake.nix
+++ flake.nix
@@ -5,42 +5,66 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     fenix,
+    naersk,
     ...
   }: let
-    pkgs = import nixpkgs {system = "x86_64-linux";};
+    system = "x86_64-linux";
+    target = "mipsel-unknown-linux-musl";
+    pkgs = import nixpkgs {inherit system;};
     mips-pkgs = import nixpkgs {
-      system = "x86_64-linux";
+      inherit system;
       crossSystem = pkgs.lib.systems.examples.mipsel-linux-gnu;
     };
   in {
-    packages.x86_64-linux.default = pkgs.dockerTools.buildLayeredImage {
-      name = "mips-cross-builder";
-      tag = "latest";
-      contents = with pkgs; [
-        bashInteractive
-        coreutils-full
-        cacert
-        stdenv.cc
-        mips-pkgs.stdenv.cc
-        (
-          with fenix.packages.x86_64-linux;
-            combine [
-              stable.rustc
-              stable.cargo
-              targets.mipsel-unknown-linux-musl.stable.rust-std
-            ]
-        )
-      ];
-      extraCommands = "mkdir -m 0777 tmp";
-      config = {
-        Cmd = ["cargo" "build" "--release"];
-        Env = ["CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse"];
-        WorkingDir = "/build";
+    packages.${system} = let
+      toolchain = with fenix.packages.${system};
+        combine [
+          stable.rustc
+          stable.cargo
+          targets.${target}.stable.rust-std
+        ];
+    in {
+      default =
+        (naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        })
+        .buildPackage {
+          src = ./.;
+          CARGO_BUILD_TARGET = target;
+          CARGO_TARGET_MIPSEL_UNKNOWN_LINUX_MUSL_LINKER = let
+            inherit (mips-pkgs.stdenv) cc;
+          in "${cc}/bin/${cc.targetPrefix}cc";
+        };
+      docker = pkgs.dockerTools.buildLayeredImage {
+        name = "mips-cross-builder";
+        tag = "latest";
+        contents = with pkgs; [
+          bashInteractive
+          coreutils-full
+          cacert
+          stdenv.cc
+          mips-pkgs.stdenv.cc
+          toolchain
+        ];
+        extraCommands = "mkdir -m 0777 tmp";
+        config = {
+          Cmd = ["cargo" "build" "--release"];
+          Env = [
+            "CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse"
+            "CARGO_BUILD_TARGET=${target}"
+          ];
+          WorkingDir = "/build";
+        };
       };
     };
   };
